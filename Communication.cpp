6801656f8b503b01beac6ed829582fcd809cf853
nicolas <nicolas.ekszterowicz@gmail.com>
2014-12-12 22:47:56 +0100

    Fin du cours d'ER, communication, traitement de la commande et du 1er parametre

--- Communication.cpp
+++ Communication.cpp
@@ -1,73 +1,74 @@
-#define DEBUG
-//#undef DEBUG
+#include "Communication.h"
 
 
-#ifdef DEBUG
-#define PRINTD Serial.println
-#else
-inline int PRINTD(char, ...)
-{
-	return 0;
-}
-inline int PRINTD(int, ...)
-{
-	return 0;
-}
-#endif
-
-#define TAILLE_CMD_TRAM 2
-
-// Ne pas modifier
-#define TAILLE_CMD_TOT TAILLE_CMD_TRAM + 1
-
-#define NB_PARAM_MAX 3
-#define TAILLE_PARAM_TRAM 3
-
-// Ne pas modifier
-#define TAILLE_PARAM_TOT TAILLE_PARAM_TRAM + 1
-
-
-
-void traitementRecep(char trameRecue[])
+void traitementRecep(char trameBrute[])
 {
 	const char carDelim = ',';
-	char commande[3] = "  ";
+	char commande[3] = "";
 	char tabParam[NB_PARAM_MAX][TAILLE_PARAM_TOT] = { "" };
 	int iBcl = 0;
 	int iCptParam = 0;
 	int iTamponDebParam = 0;
+	char trameRecue[TAILLE_TRAME_A_TRAITER_TOT] = "";
+
+	/*for (int i)*/
 
 
 	// rajouter condition sur trameRecu
-	while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_CMD_TOT)
+	// while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_CMD_TOT)
+	while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_CMD_TRAM)
 	{
 		commande[iBcl] = trameRecue[iBcl];
 		iBcl++;
 	}
 
-	while (trameRecue[iBcl] != carDelim)
+	while (trameRecue[iBcl] != carDelim && iBcl < TAILLE_TRAME_A_TRAITER)
 	{
 		iBcl++;
 	}
 
 	// On est au caractere de delimitation de la commande
-	PRINTD(trameRecue[iBcl]);
+	// PRINTD(commande);
+	// PRINTD(trameRecue[iBcl]);
 
+	// on saute ce caractere de separation
+	// iTamponDebParam correspond ici au premier caractere du 1er parametre
 	iTamponDebParam = ++iBcl;
 
+
 	// on place les parametres dans le tableau de parametres
-	if (iBcl < TAILLE_CMD_TOT)
+	// dans cette condition on doit utilisé la taille max d'une trame complète : on doit donc mettre la chaine que l'on reçoit
+	// dans une nouvelle variable dont on connait la taille (fixe)
+	if (iBcl < 50)
 	{
-		while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_CMD_TOT)
-		{
-			while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_PARAM_TOT + iTamponDebParam)
+		/*while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_CMD_TOT)
+		{*/
+			int cptChaine = 0;
+
+			// while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_PARAM_TOT + iTamponDebParam)
+			while (trameRecue[iBcl] != carDelim && trameRecue[iBcl] != '\0' && iBcl < TAILLE_PARAM_TRAM + iTamponDebParam)
 			{
-				tabParam[iCptParam][TAILLE_PARAM_TOT];
+				tabParam[iCptParam][cptChaine] = trameRecue[iBcl];
+				cptChaine++;
 				iBcl++;
 			}
 
-			iCptParam++;
-		}
+			// Cette boucle permet d'atteindre le caractere de separation des trames, si ce n'est pas deja fait
+			while (trameRecue[iBcl] != carDelim)
+			{
+				iBcl++;
+			}
+
+			// on saute ce caractere de separation
+			// iTamponDebParam correspond ici au premier caractere du 2eme parametre
+			iTamponDebParam = ++iBcl;
+
+
+			/*iCptParam++;
+		}*/
+
+			PRINTD(tabParam[0]);
+			PRINTD(trameRecue[iBcl]);
 	}
 }
 
--- Communication.h
+++ Communication.h
@@ -1,6 +1,48 @@
 #ifndef _COMMUNICATION_H_
 #define _COMMUNICATION_H_
 
+#include <Arduino.h>
+#include "Fonctions.h"
+#include "Temporisation.h"
+
+#define DEBUG
+//#undef DEBUG
+
+#ifdef DEBUG
+#define PRINTD Serial.println
+#else
+inline int PRINTD(char*, ...)
+{
+	return 0;
+}
+inline int PRINTD(char, ...)
+{
+	return 0;
+}
+inline int PRINTD(int, ...)
+{
+	return 0;
+}
+#endif
+
+
+#define TAILLE_CMD_TRAM 2
+
+// Ne pas modifier
+#define TAILLE_CMD_TOT TAILLE_CMD_TRAM + 1
+
+#define NB_PARAM_MAX 3
+#define TAILLE_PARAM_TRAM 3
+
+// Ne pas modifier
+#define TAILLE_PARAM_TOT TAILLE_PARAM_TRAM + 1
+
+
+#define TAILLE_TRAME_A_TRAITER_TOT 21
+#define TAILLE_TRAME_A_TRAITER 20
+
+
+void traitementRecep(char trameRecue[]);
 
 
 #endif
